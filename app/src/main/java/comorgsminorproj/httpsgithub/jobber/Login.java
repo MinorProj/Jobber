4f8312cca37320d61ea5f02d8f716914bd324426
Kush Garg <gargkush97@gmail.com>
2017-10-08 00:03:56 +0530

    register firebase integration

--- app/src/main/java/comorgsminorproj/httpsgithub/jobber/Login.java
+++ app/src/main/java/comorgsminorproj/httpsgithub/jobber/Login.java
@@ -4,17 +4,76 @@ package comorgsminorproj.httpsgithub.jobber;
  * Created by MK on 03-10-2017.
  */
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText emailid;
+    private  EditText passwd;
+    private Button login;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+    progressDialog = new ProgressDialog(this);
+    mAuth = FirebaseAuth.getInstance();
 
+    emailid = (EditText) findViewById(R.id.lemail);
+    passwd = (EditText) findViewById(R.id.lpassword);
+    login = (Button) findViewById(R.id.login);
+
+    }
+
+    private void loginUser(){
+
+        String email = emailid.getText().toString().trim();
+        String password = passwd.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email !", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password !", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Logging In ...");
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == login)
+            loginUser();
     }
 }
--- app/src/main/java/comorgsminorproj/httpsgithub/jobber/Register.java
+++ app/src/main/java/comorgsminorproj/httpsgithub/jobber/Register.java
@@ -1,13 +1,105 @@
 package comorgsminorproj.httpsgithub.jobber;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Register extends AppCompatActivity  implements View.OnClickListener {
+
+    private EditText uname;
+    private EditText uemail;
+    private EditText umobile;
+    private EditText upassword;
+    private Button uregister;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        uname = (EditText) findViewById(R.id.name);
+        uemail = (EditText) findViewById(R.id.email);
+        umobile = (EditText) findViewById(R.id.mobile);
+        upassword = (EditText) findViewById(R.id.password);
+        uregister = (Button) findViewById(R.id.register);
+
+        uregister.setOnClickListener(this);
     }
+        private void register() {
+
+            String email = uemail.getText().toString().trim();
+            String password = upassword.getText().toString().trim();
+            String name = uname.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "Please enter email !", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Please enter password !", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(name)) {
+                Toast.makeText(this, "Please enter your name !", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            progressDialog.setMessage("Registering User ...");
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Register.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                user.sendEmailVerification()
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(Register.this, "Email Sent", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                            } else {
+                                progressDialog.dismiss();
+                                Toast.makeText(Register.this, "Unsuccessful. Please try again !", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                    });
+        }
+
+
+
+    @Override
+    public void onClick(View v) {
+                if(v == uregister)
+                    register();
+    }
+
+
 }
