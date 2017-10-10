f8c794f4706a6fca6c6f68f77c5f8dfe9b5cfd74
swapnilkrishna <swapnilkrishna7860@gmail.com>
2017-10-10 13:41:14 +0530

    front page edited

--- app/src/main/java/comorgsminorproj/httpsgithub/jobber/Login.java
+++ app/src/main/java/comorgsminorproj/httpsgithub/jobber/Login.java
@@ -25,9 +25,8 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     private EditText emailid;
     private  EditText passwd;
     private Button login;
-
-    private FirebaseAuth mAuth;
     private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +36,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     progressDialog = new ProgressDialog(this);
     mAuth = FirebaseAuth.getInstance();
 
-    emailid = (EditText) findViewById(R.id.lemail);
+    emailid = (EditText)findViewById(R.id.lemail);
     passwd = (EditText) findViewById(R.id.lpassword);
     login = (Button) findViewById(R.id.login);
 
