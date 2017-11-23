fba1640bc14af9024bbdf92caf35dec52aa8ce20
Krrish <swapnilkrishna7860@gmail.com>
2017-11-23 11:32:15 +0530

    added loaction func in addjobs

--- app/src/main/java/comorgsminorproj/httpsgithub/jobber/AddJobs.java
+++ app/src/main/java/comorgsminorproj/httpsgithub/jobber/AddJobs.java
@@ -1,6 +1,8 @@
 package comorgsminorproj.httpsgithub.jobber;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -98,8 +100,10 @@ public class AddJobs extends AppCompatActivity {
             }
         });
     }
+
+
     }
 
 
 
-}
+
